ad1ac80f1e8109e4a66d1ce67b1a410022e843ac
Lakshya Asnani <lakshya.asnani25@gmail.com>
2025-12-23 11:08:43 +0530

    Add user registration validation and error handling with express-validator

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,14 @@
+import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error";
+
+
+
+export const validate=(req,res,next)=>{
+    const error=validateResult(req)
+    if(error.isEmpty()){
+        return next()
+    }
+    const extractedError= []
+    error.array().map((err)=>extractedError.push({[err.path]:err.msg}));
+    throw new ApiError(422,"Recieved data is not valid",extractedError);
+}
---
+++ src/validators/index.js
@@ -0,0 +1,32 @@
+import { body } from "express-validator";
+
+
+const userRegisterValidator=()=>{
+    return[
+        body("email")
+            .trim()
+            .notEmpty()
+            .withMessage("Email is Required")
+            .isEmail()
+            .withMessage("Email is invalid"),
+        body("username")
+            .trim()
+            .notEmpty()
+            .withMessage("Username is required")
+            .isLowercase()
+            .withMessage("Username must be in lower case")
+            .isLength({min:3})
+            .withMessage("Username must be atleat 3 character long"),
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password is Required"),
+            body("fullName")
+            .optional()
+            .trim()
+    ]
+}
+
+export{
+    userRegisterValidator
+}
