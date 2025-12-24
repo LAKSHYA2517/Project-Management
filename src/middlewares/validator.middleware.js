907b8a447bc948a72b0a1a7f7235de84e554cccb
Lakshya Asnani <lakshya.asnani25@gmail.com>
2025-12-24 17:31:36 +0530

    Add login functionality and cookie handling; update validation error messages

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,14 +1,14 @@
 import { validationResult } from "express-validator";
-import { ApiError } from "../utils/api-error";
+import { ApiError } from "../utils/api-error.js";
 
 
 
 export const validate=(req,res,next)=>{
-    const error=validateResult(req)
+    const error=validationResult(req)
     if(error.isEmpty()){
         return next()
     }
     const extractedError= []
     error.array().map((err)=>extractedError.push({[err.path]:err.msg}));
-    throw new ApiError(422,"Recieved data is not valid",extractedError);
+    throw new ApiError(422,"Received data is not valid",extractedError);
 }
--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -2,10 +2,11 @@ class ApiError extends Error{
     constructor(
         statusCode,
         message="Something went wrong",
+        data=null,
     ){
         super(message)
         this.statusCode=statusCode
-        this.data=null
+        this.data=data
         this.message=message
         this.success=false
 
